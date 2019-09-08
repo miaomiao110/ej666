25ad21fa4367a15cb8d8a04fa3fdcc95db858dec
miaomiao110 <983620901@qq.com>
2019-09-08 16:26:00 +0800

    send fenlei

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-// $(function(){
-// 	// 为栏目绑定事件
+$(function(){
+	// 为栏目绑定事件
 
-// 	$("ul.left_nav").on("click","li",function(){
-// 		var url = $(this).attr("url");
-// 		$("#wrapper").load(url)
-// 	})
-// });
+	$("ul.left_nav").on("click","li",function(){
+		var url = $(this).attr("url");
+		$("#wrapper").load(url)
+	})
+});
